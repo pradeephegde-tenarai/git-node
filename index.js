0ff723ae4ba4e10dd4729457f2bf0f52a5e0fcff
Pradeep <pradeep.hegde@tenarai.com>
2026-09-13 08:00:00 -0400

    Moved routing to its own directory

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
+const routes = require('./routes/routes');
 const port = 8000;
 const app = express();
 const router = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-router.get('/', (request, response) => response.send('hello from skillsoft'));
-
-router.post('/addemployee', (request, response) => {
-    let empName = request.body.empName;
-    let empPass = request.body.empPass;
-    response.end(`POST success, you sent ${empName} and ${empPass},
-thanks!`);
-});
+routes(router)
 
 app.use('/', router);
 
