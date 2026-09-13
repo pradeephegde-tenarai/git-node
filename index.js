a4d3561ab46ccec0d77f2c5c4deabc8bceced587
Pradeep <pradeep.hegde@tenarai.com>
2026-09-13 07:54:22 -0400

    Add Express Router, initial routes and a POST route

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const port = 8000;
 const app = express();
-app.get('/', (request, response) => response.send('hello from skillsoft'));
+const router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+router.get('/', (request, response) => response.send('hello from skillsoft'));
+
+router.post('/addemployee', (request, response) => {
+    let empName = request.body.empName;
+    let empPass = request.body.empPass;
+    response.end(`POST success, you sent ${empName} and ${empPass},
+thanks!`);
+});
+
+app.use('/', router);
+
 app.listen(port, function () {
     console.log("Listening " + port);
 });
